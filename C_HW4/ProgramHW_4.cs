69bd5379e54871584564402161030b13c8455383
EduardUrakov <eduard.urakov@mail.ru>
2022-12-13 20:56:26 +0500

    ferst exemple

--- C_HW4/ProgramHW_4.cs
+++ C_HW4/ProgramHW_4.cs
@@ -1,46 +1,24 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-void NumDeg(int num, int step)
+void NumDegree(int num, int step)
 {
-int result = num;
-if(step == 0)
-{
-    result = 0;
-    Console.WriteLine(result);
-}
-else 
-    for(int size = 1; size <= step; size++)
+    int result = 1;
+    
+    for(int i = 1; i <= step; i++)
     {
         result = result * num;
-        Console.WriteLine(result);
     }
-    for(int size =1; size >= step; size--)
-    {
-        result = result / num;
-        Console.WriteLine(result);
-    }
+    Console.WriteLine("The result is: " + result);
 }
+
 Console.WriteLine("Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a degree of number: ");
 int step = Convert.ToInt32(Console.ReadLine());
 
+NumDegree(num, step);
+Console.WriteLine(); 
 
-Console.WriteLine(NumDeg);
-
-/* 
-Console.WriteLine("введите число A");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число B");
-int b = Convert.ToInt32(Console.ReadLine());
-int step = a;
-
-for (int i = 1; i < b; i++)
-{
-step = step * a;
-}
-Console.WriteLine("A в степени B равно: " + step);
- */
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
